d61d2bf3bd7224d145c55d8bf8a1870cd90ddf5c
Maria <maria.altyeva@gmail.com>
2020-03-09 19:05:24 +0900

    basic controllers completed

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,12 @@
 class CocktailsController < ApplicationController
+  before_action :authenticate_user!, only: :toggle_favorite
+
   def index
     @cocktails = Cocktail.all
+  end
+
+  def toggle_favorite
+    @cocktail = Cocktail.find(params[:id])
+    current_user.favorited?(@cocktail) ? current_user.unfavorite(@cocktail) : current_user.favorite(@cocktail)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root 'cocktails#index'
+  devise_for :users
+
   resources :cocktails, only: :index do
     member do
       post 'toggle_favorite', to: "cocktails#toggle_favorite"
     end
   end
-  devise_for :users
-  root 'static#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
